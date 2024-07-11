324498f1915eeacc9f33193a759a91eec59a5fe7
Mike3247 <72448458+Mike3247@users.noreply.github.com>
2024-07-11 16:26:52 -0400

    develop basic routing system

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 require('dotenv').config()
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 // const url = require('url');
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    res.write("Hello World");
-    res.end();
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += 'error404.html';
+            break;
+    };
+
+    // send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
+
 });
 
 server.listen((PORT), () => {
